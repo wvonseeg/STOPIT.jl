340d01e5233161af82a47102120208d2f6166078
wvonseeg <116127888+wvonseeg@users.noreply.github.com>
2023-06-13 20:51:58 -0400

    Added function that prints out information about the particle

--- src/particle.jl
+++ src/particle.jl
@@ -6,3 +6,9 @@ struct Particle
 end
 
 # Constructor to create particle by looking up mass from table
+
+function info(part::Particle)
+    print("Z = ", part.Z)
+    print("A = ", part.A)
+    print("E = ", part.energy, " MeV")
+end
