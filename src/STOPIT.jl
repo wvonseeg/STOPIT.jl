dd1bc7dfbc7dd4f57baf7b9702ca006f9ea1d661
wvonseeg <116127888+wvonseeg@users.noreply.github.com>
2023-06-14 18:50:42 -0400

    Added use of Unitful to help keep track of...units

--- src/STOPIT.jl
+++ src/STOPIT.jl
@@ -3,6 +3,7 @@ module STOPIT
 using StaticArrays
 using ArgCheck
 
+include("units.jl")
 include("particle.jl")
 include("desorb/desorb.jl")
 
---
+++ src/units.jl
@@ -0,0 +1,29 @@
+using Unitful
+
+import Unitful:
+    nm, μm, mm, cm, m,
+    μm, mg, g, kg,
+    Ra, °F, °C, K,
+    rad, °,
+    ns, μs, ms, s, minute, hr, d,
+    Hz, kHz, MHz,
+    J, eV, keV, MeV,
+    nA, μA, mA, A, mol,
+    μW, mW, W, C,
+    mTorr, Torr, atm, bar, mbar, Pa, kPa
+
+import Unitful:
+    c0, mn, mp, q
+
+import Unitful: 𝐋, 𝐌, 𝐓, 𝐈, 𝐉, 𝐍, 𝚯
+
+import Unitful:
+    Length, Area, Volume,
+    Velocity, Luminosity, Density,
+    Time, Frequency, Mass,
+    Current, Charge, Power,
+    Temperature, AbsoluteScaleTemperature, RelativeScaleTemperature
+
+import Unitful: LengthUnits, AreaUnits, MassUnits, TemperatureUnits
+
+@derived_dimension Thickness 𝐌 / 𝐋^2 true
