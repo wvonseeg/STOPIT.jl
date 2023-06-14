b12f5bd2dfb5e56c3226998c252368fe34fdf2f3
wvonseeg <116127888+wvonseeg@users.noreply.github.com>
2023-06-13 21:26:33 -0400

    Added list of standardized media included in STOPIT

---
+++ src/desorb/standardmedia.jl
@@ -0,0 +1,24 @@
+const global STANDARDMEDIA = ("C02", "Si", "Graphite", "C4H10", "CF4", "CH2", "CD2", "He", "H2")
+
+function getstandardmedium(name::String; pressure::Float64=0.0, depth::Float64=0.0)
+    @argcheck name in STANDARDMEDIA
+    if name == "C02"
+        return GasAbsorber((12, 16), (6, 8), (1, 2), pressure, depth)
+    elseif name == "Si"
+        return AbsorberLayer((28,), (14,), (1,), 0.2329 * depth, 2.329)
+    elseif name == "Graphite"
+        return AbsorberLayer((12,), (6,), (1,), 0.2267 * depth, 2.267)
+    elseif name == "C4H10"
+        return GasAbsorber((12, 1), (6, 1), (4, 10), pressure, depth)
+    elseif name == "CF4"
+        return GasAbsorber((12, 19), (6, 9), (1, 4), pressure, depth)
+    elseif name == "CH2"
+        return AbsorberLayer((12, 1), (6, 1), (1, 2), 0.094 * depth, 0.94)
+    elseif name == "CD2"
+        return AbsorberLayer((12, 2), (6, 1), (1, 2), 0.094 * depth, 0.94)
+    elseif name == "He"
+        return GasLayer((4,), (2,), (1,), pressure, depth)
+    elseif name == "H2"
+        return GasLayer((1,), (1,), (2,), pressure, depth)
+    end
+end
