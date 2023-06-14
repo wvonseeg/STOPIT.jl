e3eedef855cb5b0d3849ba2cc96d737b772874a4
wvonseeg <116127888+wvonseeg@users.noreply.github.com>
2023-06-13 20:48:33 -0400

    Added particle struct for energy loss calculations

--- src/STOPIT.jl
+++ src/STOPIT.jl
@@ -3,6 +3,7 @@ module STOPIT
 using StaticArrays
 using ArgCheck
 
+include("particle.jl")
 include("desorb/desorb.jl")
 
 end # module STOPIT
---
+++ src/particle.jl
@@ -0,0 +1,8 @@
+struct Particle
+    Z::Integer
+    Anumber::Integer
+    A::Float64
+    energy::Float64
+end
+
+# Constructor to create particle by looking up mass from table
