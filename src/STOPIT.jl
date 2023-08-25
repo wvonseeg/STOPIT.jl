ac1cad86f5c446d54294a1ef3ab597eee35fbebe
wvonseeg <116127888+wvonseeg@users.noreply.github.com>
2023-08-25 17:24:18 -0400

    created new folder to hold files that provide general utility

--- src/STOPIT.jl
+++ src/STOPIT.jl
@@ -3,8 +3,8 @@ module STOPIT
 using StaticArrays
 using ArgCheck
 
-include("units.jl")
-include("errors.jl")
+include("utils/units.jl")
+include("utils/errors.jl")
 include("particle.jl")
 include("absorbers/absorbers.jl")
 include("desorb/desorb.jl")
