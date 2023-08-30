b3ac6795a9149dea4c10bb20e1109d1eee048975
wvonseeg <116127888+wvonseeg@users.noreply.github.com>
2023-08-29 22:29:19 -0400

    Fixed Q-values calculations

--- src/Particles/reactions.jl
+++ src/Particles/reactions.jl
@@ -8,15 +8,15 @@ function Qvalue(target::Particle, projectile::Particle, rxntype::String)
         productmass = getmass(productA, productZ)[1, 1]
     elseif rxntype == "nn"
         productA -= 1
-        productmass = getmass(productA, productZ)[1, 1]
+        productmass = getmass([productA,1], [productZ,0])[1, 1]
     elseif rxntype == "p"
         productA -= 1
         productZ -= 1
-        productmass = getmass(productA, productZ)[1, 1]
+        productmass = getmass([productA,1], [productZ,1])[1, 1]
     elseif rxntype == "a"
         productA -= 4
         productZ -= 2
-        productmass = getmass(productA, productZ)[1, 1]
+        productmass = getmass([productA,4], [productZ,2])[1, 1]
     end
     return masstoenergy(target.mass + projectile.mass - productmass)
 end
