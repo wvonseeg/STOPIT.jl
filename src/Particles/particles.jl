d7b83d3b48ce60549d0c4775a1b7f3df5767052a
wvonseeg <116127888+wvonseeg@users.noreply.github.com>
2023-09-03 18:04:26 -0400

    Added atomic symbol to particle struct

--- src/Particles/particles.jl
+++ src/Particles/particles.jl
@@ -2,24 +2,33 @@ module Particles
 using NaturallyUnitful, ArgCheck
 import ..STOPIT: getmass
 export Particle, info, Qvalue
+
+const atomicsymbols = ("n", "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
+    "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn",
+    "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr",
+    "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs",
+    "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+    "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At",
+    "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
+    "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc",
+    "Lv", "Ts", "Og")
+
 struct Particle
     A::UInt16
     mass::typeof(1.0u"u")
     Z::UInt8
     energy::typeof(1.0u"MeV")
+    symbol::String
 end
 
 # Constructor to create particle by looking up mass from table
 function Particle(A::Integer, Z::Integer, energy::typeof(1.0u"MeV"))
     mass = getmass(Z, A)
-    Particle(Z, A, mass[1, 1], energy)
+    Particle(A, mass[1, 1], Z, energy, atomicsymbols[Z+1])
 end
 
-function show(part::Particle)
-    println("Z = ", part.Z)
-    println("A = ", part.A)
-    println("Mass = ", part.mass)
-    println("E = ", part.energy)
+function Base.show(io::IO, ::MIME"text/plain", x::Particle)
+    println("$(x.A)$(x.symbol) ($(x.mass)) @ $(x.energy)")
 end
 
 include("reactions.jl")
